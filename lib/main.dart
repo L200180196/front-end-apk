e58164033fccac1128c1cc50325d6b20f8acd6da
L200180194 <alfiantoandy6@gmail.com>
2021-05-19 13:36:15 +0700

    revisi splash screen , versi sblm direvisi di main.dart dan hanya di comment

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
       providers: [BlocProvider(create: (_) => UserCubit())],
       child: GetMaterialApp(
         debugShowCheckedModeBanner: false,
-        home: Splash2(),
+        home: SignInPage(),
         theme: ThemeData(
             textTheme: TextTheme(
                 title: TextStyle(
@@ -33,36 +33,37 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Splash2 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var orientation = MediaQuery.of(context).orientation;
-    return Column(
-      children: [
-        Expanded(
-          flex: orientation == Orientation.portrait ? 3 : 3,
-          child: SizedBox(
-            // height: MediaQuery.of(context).size.height * 0.2,
-            child: Container(
-              color: Colors.white,
-            ),
-          ),
-        ),
-        Expanded(
-          flex: orientation == Orientation.portrait ? 7 : 6,
-          child: Container(
-            // height: MediaQuery.of(context).size.height * 0.7,
-            child: SplashScreen(
-              seconds: 6,
-              navigateAfterSeconds: new SignInPage(),
-              image: new Image.asset("assets/jogjakarir.png"),
-              photoSize: orientation == Orientation.portrait ? 100.0 : 65,
-              useLoader: false,
-              loaderColor: Colors.transparent,
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-}
+///splash screen tidak dipakai atau opsi ke 2
+// class Splash2 extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     var orientation = MediaQuery.of(context).orientation;
+//     return Column(
+//       children: [
+//         Expanded(
+//           flex: orientation == Orientation.portrait ? 3 : 3,
+//           child: SizedBox(
+//             // height: MediaQuery.of(context).size.height * 0.2,
+//             child: Container(
+//               color: Colors.white,
+//             ),
+//           ),
+//         ),
+//         Expanded(
+//           flex: orientation == Orientation.portrait ? 7 : 6,
+//           child: Container(
+//             // height: MediaQuery.of(context).size.height * 0.7,
+//             child: SplashScreen(
+//               seconds: 6,
+//               navigateAfterSeconds: new SignInPage(),
+//               image: new Image.asset("assets/jogjakarir.png"),
+//               photoSize: orientation == Orientation.portrait ? 100.0 : 65,
+//               useLoader: false,
+//               loaderColor: Colors.transparent,
+//             ),
+//           ),
+//         ),
+//       ],
+//     );
+//   }
+// }
